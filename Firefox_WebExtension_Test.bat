a15ba9ca47153f4b0725c75cfd1688a74d43aa50
t-ryutada <rtada@tech.softbank.co.jp>
2017-11-16 04:04:20 +0900

    バッチの動作内容を修正

--- Firefox_WebExtension_Test.bat
+++ Firefox_WebExtension_Test.bat
@@ -6,11 +6,12 @@ set time_tmp=%time: =0%
 set now=%date:/=%%time_tmp:~0,2%%time_tmp:~3,2%%time_tmp:~6,2%
 SET RND=%RANDOM%
 
-REM mkdir "test-"%now%"-"%RND%"-"%saveDirectry%
-REM dir > result-%now%.txt
+REM フォルダを作成する処理
+dir > test-%now%-%RND%.txt
 
-echo %now%:%RND% > cmdResultTest.txt
-echo %0 : %1 : %2 : %3 : %4 : %5 >> cmdResultTest.txt
+REM cmdResultTest.txtを作成し中見を更新する
+REM echo %now%:%RND% > cmdResultTest.txt
+REM echo %0 : %1 : %2 : %3 : %4 : %5 >> cmdResultTest.txt
 
 REM test-%now%-%saveDirectry% > result-%RND%.txt
 REM pause >nul
